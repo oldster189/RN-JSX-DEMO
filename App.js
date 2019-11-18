8eafa52d6e0cf341ee31a0979cedbc3f4bf31a89
oldster189 <oldster189@gmail.com>
2019-11-19 01:28:55 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Demo JSX App</Text>
+      {/* 1. Nested Elements */}
+
+      {/* 2. Attributes */}
+
+      {/* 3. JavaScript Expressions */}
+
+      {/* 4. Styling */}
+
+      {/* 5. Comments */}
+
+      {/* 6. Naming Convention */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 40,
+  },
+});
+
+export default App;
